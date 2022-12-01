39ba0b4e4bdbd3fe93219cda2395436bc88bc7dc
Bharadwaj Balraj <ase.bharath@gmail.com>
2022-12-01 17:58:51 +0530

    More examples
    Adding more examples to confuse

--- Singleton.playground/Contents.swift
+++ Singleton.playground/Contents.swift
@@ -7,3 +7,18 @@ final class SingletonFinal {
     static let shared = SingletonFinal()
     private init() {}
 }
+
+class SingletonLet {
+    static let shared = SingletonLet()
+    private init() {}
+}
+
+class SingletonVar {
+    static var shared = SingletonVar()
+    private init() {}
+}
+
+class SingletonExposed {
+    static let shared = SingletonExposed()
+    init() {}
+}
